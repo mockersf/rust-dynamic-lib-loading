09f717fbcb904ed5014ac7d4c7e6dbf158a979f1
François Mockers <mockersf@gmail.com>
2017-05-31 01:44:14 +0200

    shorter example

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,16 @@
 extern crate libloading as lib;
 
-pub struct LoadedModule {
-    lib: lib::Library,
-    compute: lib::os::unix::Symbol<extern "Rust" fn(&str) -> String>,
-}
-
-impl LoadedModule {
-    pub fn load(path: &str) -> LoadedModule {
-        let lib = lib::Library::new(path).unwrap();
-        let compute = unsafe {
-            let func: lib::Symbol<extern "Rust" fn(&str) -> String> =
-                lib.get(b"compute\0").unwrap();
-            func.into_raw()
-        };
-        println!("Loaded {:?}", path);
-        LoadedModule {
-            lib: lib,
-            compute,
-        }
-    }
-
-    fn compute(&self, req: &str) -> String {
-        let compute = &self.compute;
-        compute(req)
-    }
-}
-
 fn main() {
-    let module_path = "module1/target/debug/libmodule1.dylib";
-    let module1 = LoadedModule::load(module_path);
+    let path = "module1/target/debug/libmodule1.dylib";
     
+    let lib = lib::Library::new(path).unwrap();
+    let compute = unsafe {
+        let func: lib::Symbol<extern "Rust" fn(&str) -> String> =
+            lib.get(b"compute\0").unwrap();
+        func.into_raw()
+    };
     for i in 0..10000 {
-        println!("{}", module1.compute(&format!("{}", i)));
+        println!("{}", compute(&format!("{}", i)));
     }
 
 }
